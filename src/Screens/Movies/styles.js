e02b13613828d99b51fe55465cf0904a8a13a4a1
Flávia Correia <fllaviacorreia@outlook.com>
2023-08-19 16:45:20 -0300

    update styles.js of movies screen

--- src/Screens/Movies/styles.js
+++ src/Screens/Movies/styles.js
@@ -1,7 +1,14 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+    container: { 
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        width: '100%', 
+        padding: 10,
+        backgroundColor:'#777777',
+    },
 });
 
 export default styles;
