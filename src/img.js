8156b923de6288d14e9868d3b8b046288e6df805
Flávia Correia <fllaviacorreia@outlook.com>
2023-08-20 09:36:10 -0300

    trying another imgs

---
+++ src/img.js
@@ -0,0 +1,15 @@
+var images = [
+    require('../assets/movies/0.jpg'),
+    require('../assets/movies/1.webp'),
+    require('../assets/movies/2.jpg'),
+    require('../assets/movies/3.jpg'),
+    require('../assets/movies/4.webp'),
+    require('../assets/movies/5.jpg'),
+    require('../assets/movies/6.jpg'),
+    require('../assets/movies/7.webp'),
+    require('../assets/movies/8.jpg'),
+    require('../assets/movies/9.jpg'),
+    require('../assets/movies/10.jpg'),
+];
+
+export default images;
